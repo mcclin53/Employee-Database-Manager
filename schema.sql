4c39c5fcc753fcd9821bee40418e45f0842e07f1
mcclin53 <dan.mcclintic@gmail.com>
2025-03-07 23:20:57 -0500

    updated schema file

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS ;
+CREATE DATABASE ;
+
+/c ;
+
+CREATE TABLE employees(
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+)
+
+CREATE TABLE departments(
+    department_id SERIAL PRIMARY KEY,
+    department_name VARCHAR (30) UNIQUE NOT NULL,
+)
+
+CREATE TABLE roles(
+    role_id SERIAL PRIMARY KEY, 
+    job_title VARCHAR (30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+)
