1735095d7a0036568e880dbe1b79a9d09f774f03
mcclin53 <dan.mcclintic@gmail.com>
2025-03-09 17:08:03 -0400

    added main.ts code

--- db/main.ts
+++ db/main.ts
@@ -1,4 +1,68 @@
 import inquirer from 'inquirer';
+import { pool } from '../src/connection.js';
+
+pool.connect();
+
+const displayTable = async (tableName) => {
+    try {
+        const res = await pool.query(`SELECT * FROM ${tableName}`);
+        console.table(res.rows);
+    } catch (err) {
+        console.error('Error executing query');
+    }
+};
+
+function displayDepartments = pool.query('SELECT * FROM departments', (err: Error, result: QueryResult) => {
+  if (err) {
+    console.log(err);
+  } else if (result) {
+    console.log(result.rows);
+  }
+});
+
+function displayRoles = pool.query('SELECT * FROM roles', (err: Error, result: QueryResult) => {
+    if (err) {
+      console.log(err);
+    } else if (result) {
+      console.log(result.rows);
+    }
+  });
+
+  function displayEmployees {
+    pool.query('SELECT * FROM employees', (err: Error, result: QueryResult) => {
+    if (err) {
+      console.log(err);
+    } else if (result) {
+      console.log(result.rows);
+    }
+  });
+
+function addDepartment() {
+    pool.query('INSERT INTO departments department_name', (err: Error, result: QueryResult) => {
+        if (err) {
+          console.log(err);
+        } else if (result) {
+          console.log(result.rows);
+
+});
+
+function addROle() {
+    pool.query('INSERT INTO roles role_id, job_title, salary, department_id', (err: Error, result: QueryResult) => {
+        if (err) {
+          console.log(err);
+        } else if (result) {
+          console.log(result.rows);
+
+});
+
+function addEmployee() {
+    pool.query('INSERT INTO employees first_name, last_name, role_id, ', (err: Error, result: QueryResult) => {
+        if (err) {
+          console.log(err);
+        } else if (result) {
+          console.log(result.rows);
+
+});
 
 inquirer
     .prompt([
@@ -7,36 +71,22 @@ inquirer
             message: 'What would you like to do?',
             name: 'menu',
             choices: ['View all departments','View all roles','View all employees','Add a department','Add a role', 'Add an employee', 'Update an employee role'],
-        },
-        {
-            type: 'confirm',
-            message: 'deptTable',
-            name: 'department table',
-            default: true
-        },
-        {
-            type: 'confirm',
-            message: 'roleTable',
-            name: 'roles table',
-            default: true
-        },
-        {
-            type: 'confirm',
-            message: 'employeeTable',
-            name: 'employee table',
-            default: true
-        },
+        }    
     ])
     .then((answer) =>
-        if (answer.deptTable) {
-            displayTable(departments)
-        } else if {
-            (answer.roleTable) {
-                displayTable(roles)
-            } else if {
-                (answer.employeeTable) {
-                    displayTable(employees)
-                }
-            }
+        switch (answer.menu) {
+            case 'View all departments';
+                return displayDepartments;
+            case 'View all roles';
+                return displayRoles;
+            case 'View all employees';
+                return displayEmployees;
+            case 'Add a department';
+                return addDepartment;
+            case 'Add a role';
+                return addROle;
+            case 'Add an employee';
+                return addEmployee;
+
         }
-    ));
+    )};
--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
 DROP DATABASE IF EXISTS employeeManager_db;
 CREATE DATABASE employeeManager_db;
 
-/c employeeManager_db;
+\c employeeManager_db;
 
 CREATE TABLE employees(
     employee_id SERIAL PRIMARY KEY,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-)
+    manager_id INTEGER
+);
 
 CREATE TABLE departments(
     department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR (30) UNIQUE NOT NULL,
-)
+    department_name VARCHAR (30) UNIQUE NOT NULL
+);
 
 CREATE TABLE roles(
     role_id SERIAL PRIMARY KEY, 
     job_title VARCHAR (30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-)
+    department_id INTEGER NOT NULL
+);
